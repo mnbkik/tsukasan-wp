793d7c42b3d4361b4909777a730ef6f3ed0819b2
KeikoMinobe <minokei-hkt5@jcom.home.ne.jp>
2024-11-13 23:40:29 +0900

    ハンバーガー追加

--- js/script.js
+++ js/script.js
@@ -2,27 +2,48 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("button").addEventListener("click", function () {
     document.querySelector(".c-hamburger").classList.toggle("is-active")
-    console.log("test")
+    document.querySelector(".c-drawer").classList.toggle("is-active");
+    document.body.classList.toggle("no-scroll"); // スクロールを無効化
   });
 })
 
-//swiper
-const swiper1 = new Swiper(".swiper1", {
-  loop: true, // デフォルトはfalse
-  autoplay: { // 自動再生
-    delay: 3000, // 3秒後に次のスライド（初期値：3000）
-    disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
-  }
-});
-
-const swiper2 = new Swiper(".swiper2", {
-  loop: true, // デフォルトはfalse
-  slidesPerView: 2,// 一度に表示するスライド数（初期値：1）
-  autoplay: { // 自動再生
-    delay: 3000, // 3秒後に次のスライド（初期値：3000）
-    disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
-  }
-});
 
 
+
+
+    //swiper
+    const swiper1 = new Swiper(".swiper1", {
+      loop: true, // デフォルトはfalse
+      autoplay: { // 自動再生
+        delay: 3000, // 3秒後に次のスライド（初期値：3000）
+        disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
+      }
+    });
+
+
+
+
+    const swiper2 = new Swiper(".swiper2", {
+      loop: true, // デフォルトはfalse
+      slidesPerView: 2,// 一度に表示するスライド数（初期値：1）
+      autoplay: { // 自動再生
+        delay: 3000, // 3秒後に次のスライド（初期値：3000）
+        disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
+      },
+      spaceBetween: 20 ,// 画面幅768px未満では20px
+      breakpoints: {
+        768: {
+          spaceBetween: 40, // 画面幅768px未満では20px
+        },
+      },
+    });
+
+
+    const swiper3 = new Swiper(".swiper3", {
+      loop: true, // デフォルトはfalse
+      autoplay: { // 自動再生
+        delay: 3000, // 3秒後に次のスライド（初期値：3000）
+        disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
+      }
+    });
 
